70caaa104fc1d687c759898a13d4a5595392852b
Your Name <gim.kobayashi@gmail.com>
2018-01-27 12:14:36 +0900

    いくつかの関数追加

---
+++ 10-prepare.py
@@ -0,0 +1,59 @@
+import math
+
+
+# サイクロイド
+"""
+x = a ( theta  - sin theta )
+y = a ( theta  - cos theta )
+"""
+
+pairs = []
+for i in range(10000):
+  theta = i/100.0
+  a = 1
+  x = a * ( theta - math.sin(theta) )
+  y = a * ( 1. - math.cos(theta) )
+  pairs.append( (x, y) )
+
+for pair in pairs:
+  x, y = pair
+  #print(x, y)
+
+# アステロイド
+pairs = []
+for i in range(100):
+  theta = i/10.0
+  a = 1
+  x = a * ( math.pow(math.sin(theta),3) )
+  y = a * ( math.pow(math.cos(theta),3) )
+  pairs.append( (x, y) )
+
+for pair in pairs:
+  x, y = pair
+  #print(x, y)
+
+# カージオイド
+pairs = []
+for i in range(100):
+  theta = i/10.0
+  a = 1
+  x = a * ( 1. + math.cos(theta) ) * math.cos(theta)
+  y = a * ( 1. + math.cos(theta) ) * math.sin(theta)
+  pairs.append( (x, y) )
+
+for pair in pairs:
+  x, y = pair
+  print(x, y)
+
+# 対数らせん
+pairs = []
+for i in range(100):
+  theta = i/20.0
+  a = 1
+  x = a * ( math.pow(math.e, theta/(2*math.pi) ) ) * math.cos(theta)
+  y = a * ( math.pow(math.e, theta/(2*math.pi) ) ) * math.sin(theta)
+  pairs.append( (x, y) )
+
+for pair in pairs:
+  x, y = pair
+  print(x, y)
