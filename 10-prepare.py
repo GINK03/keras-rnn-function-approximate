ee09c90b87aefe8bf567cb249f7b1549205d3d99
Your Name <gim.kobayashi@gmail.com>
2018-01-28 20:07:36 +0900

    いくつかの関数を追加

--- 10-prepare.py
+++ 10-prepare.py
@@ -61,33 +61,42 @@ for i in range(len(pairs)-10):
   y = list(pairs[i+9])
   datasets.append( (X1, X2, y) )
 open('カージオイド.pkl', 'wb').write( pickle.dumps( datasets ) )
-sys.exit()
+#sys.exit()
 
 # 対数らせん
 pairs = []
-for i in range(100):
-  theta = i/20.0
+for i in range(-10000, 80000):
+  theta = i/5000.0
   a = 1
   x = a * ( math.pow(math.e, theta/(2*math.pi) ) ) * math.cos(theta)
   y = a * ( math.pow(math.e, theta/(2*math.pi) ) ) * math.sin(theta)
   pairs.append( (x, y) )
 
-for pair in pairs:
-  x, y = pair
-  print(x, y)
+datasets = []
+for i in range(len(pairs)-10):
+  X1 = [y for x,y in pairs[i:i+9]]
+  X2 = [y for x,y in pairs[i:i+9]]
+  y = list(pairs[i+9])
+  datasets.append( (X1, X2, y) )
+open('対数らせん.pkl', 'wb').write( pickle.dumps( datasets ) )
 
 # リサージュ曲線
 pairs = []
-for i in range(100):
-  theta = i/20.0
+for i in range(-100000,100000):
+  theta = i/1000.0
   a = 1
   x = a * math.sin(3. * theta + 0.0)
   y = a * math.cos(4. * theta)
   pairs.append( (x, y) )
 
-for pair in pairs:
-  x, y = pair
-  print(x, y)
+datasets = []
+for i in range(len(pairs)-10):
+  X1 = [y for x,y in pairs[i:i+9]]
+  X2 = [y for x,y in pairs[i:i+9]]
+  y = list(pairs[i+9])
+  datasets.append( (X1, X2, y) )
+open('リサージュ曲線.pkl', 'wb').write( pickle.dumps( datasets ) )
+
 
 # インボリュート
 pairs = []
